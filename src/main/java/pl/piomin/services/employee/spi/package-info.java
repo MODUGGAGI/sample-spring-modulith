8dd4dd182c123887211c6be6ceb70ff0515ff2cd
MODUGGAGI <kiroro0814@naver.com>
2025-11-07 17:07:30 +0900

    feat(package-info): 패키지 이름 정의
    NamedInterface를 사용하여 패키지의 이름을 정의해서 외부에 공개하도록 했습니다.

---
+++ src/main/java/pl/piomin/services/employee/spi/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * NamedInterface 사용하기
+ * 참고 사이트
+ * https://docs.spring.io/spring-modulith/reference/fundamentals.html#modules.explicit-dependencies
+ */
+@org.springframework.modulith.NamedInterface("spi")
+package pl.piomin.services.employee.spi;
